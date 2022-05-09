7e437f70debc81e4eb99a9783619b85a19581f91
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 15:59:46 -0700

    working vercel

---
+++ api/capitol_of.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+# from urllib import parse
+# import requests
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = 'I am alive!'
+        self.wfile.write(message.encode())
+        return
