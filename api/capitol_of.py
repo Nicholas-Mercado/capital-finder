38ec8ea7b3e4733b3c58df8c152a4377b614a4ea
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 16:33:38 -0700

    parse query to dict

--- api/capitol_of.py
+++ api/capitol_of.py
@@ -1,14 +1,18 @@
 from http.server import BaseHTTPRequestHandler
-# from urllib import parse
+from urllib import parse
 # import requests
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-
+        
+        url_parts = parse.urlsplit(self.path)
+        query_string = parse.parse_qsl(url_parts.query)
+        dic = dict(query_string)
+         
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = 'I am alive!'
-        self.wfile.write(message.encode())
+        msg = f'{dic}'
+        self.wfile.write(msg.encode())
         return
