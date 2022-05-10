2933d2bc529ac58e7084ef665938e5431ca5598f
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 18:18:32 -0700

    working The capital of X is Y

--- api/capitol_of.py
+++ api/capitol_of.py
@@ -9,26 +9,20 @@ class handler(BaseHTTPRequestHandler):
         url_parts = parse.urlsplit(self.path)
         query_string = parse.parse_qsl(url_parts.query)
         dic = dict(query_string)
-        country = dic.get('country')
+        country = dic.get('name')
         capital = dic.get('capital')
         
-        if 'name' in dic:
+        if country:
             url = 'https://restcountries.com/v3.1/name/'
-            print('first url: ', url)
             full_url = url + dic['name']
-            print('full url',full_url)
             request = requests.get(full_url)
-            print('my request: ',request)
             data = request.json()
-            print(data)
-            capitol = data[0]['capital']
-            print(capitol)
-            msg = str(capitol)
-            print(msg)
-            # print("should be capitol: ", msg)
-            mssg ='test'   
+            capital = str(data[0]['capital'][0])
+            
+            msg = f'The capital of {country} is {capital}' 
             self.send_response(200)
             self.send_header('Content-type','text/plain')
             self.end_headers()
-            self.wfile.write(mssg.encode())
+            self.wfile.write(msg.encode())
             return
+
