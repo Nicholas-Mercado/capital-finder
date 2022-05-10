95d3c1e1e6e06d63b4e240a74132c8680a825c5e
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 17:56:26 -0700

    return dict info

--- api/capitol_of.py
+++ api/capitol_of.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-# import requests
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
@@ -9,10 +9,26 @@ class handler(BaseHTTPRequestHandler):
         url_parts = parse.urlsplit(self.path)
         query_string = parse.parse_qsl(url_parts.query)
         dic = dict(query_string)
-         
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        msg = f'{dic}'
-        self.wfile.write(msg.encode())
-        return
+        country = dic.get('country')
+        capital = dic.get('capital')
+        
+        if 'name' in dic:
+            url = 'https://restcountries.com/v3.1/name/'
+            print('first url: ', url)
+            full_url = url + dic['name']
+            print('full url',full_url)
+            request = requests.get(full_url)
+            print('my request: ',request)
+            data = request.json()
+            print(data)
+            capitol = data[0]['capital']
+            print(capitol)
+            msg = str(capitol)
+            print(msg)
+            # print("should be capitol: ", msg)
+            mssg ='test'   
+            self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write(mssg.encode())
+            return
