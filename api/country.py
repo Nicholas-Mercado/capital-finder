be60ab72678a1b3413d92c0f7483606e069cbdff
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 19:37:18 -0700

    returning data

---
+++ api/country.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        
+        url_parts = parse.urlsplit(self.path)
+        query_string = parse.parse_qsl(url_parts.query)
+        dic = dict(query_string)
+        
+        if 'name' in dic:
+            
+            url = "https://restcountries.com/v3.1/capital/"
+            full_url = url + dic['name']
+            request = requests.get(full_url)
+            data = request.json()
+            print(data)
+
+            
+            
+            msg = 'test'
+            self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write(msg.encode())
