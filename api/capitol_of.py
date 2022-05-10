625e98270927ecf1d44cf23576b08e3f746b6162
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 19:24:49 -0700

    refactor

--- api/capitol_of.py
+++ api/capitol_of.py
@@ -9,20 +9,21 @@ class handler(BaseHTTPRequestHandler):
         url_parts = parse.urlsplit(self.path)
         query_string = parse.parse_qsl(url_parts.query)
         dic = dict(query_string)
-        country = dic.get('name')
-        capital = dic.get('capital')
-        
-        if country:
+
+        if 'name' in dic:
+            
             url = 'https://restcountries.com/v3.1/name/'
             full_url = url + dic['name']
+            print(full_url)
             request = requests.get(full_url)
             data = request.json()
             capital = str(data[0]['capital'][0])
-            
+            country = dic['name'].capitalize()
             msg = f'The capital of {country} is {capital}' 
             self.send_response(200)
             self.send_header('Content-type','text/plain')
             self.end_headers()
             self.wfile.write(msg.encode())
-            return
+            
 
+    
